6cf6d1341d89b87e8f7559ea7d3ed1a6d86239d8
Andrey <k-udm@ya.ru>
2021-03-01 20:57:39 +0400

    Java 3 Lesson 5 Homework

--- Java_level_3/lesson_5/src/lesson_5/Car.java
+++ Java_level_3/lesson_5/src/lesson_5/Car.java
@@ -28,7 +28,7 @@ public class Car implements Runnable {
         this.name = "Участник #" + CARS_COUNT;
     }
     @Override
-    public synchronized void run() {
+    public void run() {
         try {
             System.out.println(this.name + " готовится");
             Thread.sleep(500 + (int)(Math.random() * 800));
@@ -50,9 +50,11 @@ public class Car implements Runnable {
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
-        if (!isWinnerFinished) {
-            winnerName = this.name;
-            isWinnerFinished = true;
+        synchronized (monitor) {
+            if (!isWinnerFinished) {
+                winnerName = this.name;
+                isWinnerFinished = true;
+            }
         }
     }
 }
