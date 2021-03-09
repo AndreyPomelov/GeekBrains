54dd53305946fdef4d23d00363d62700d9b0d810
AndreyPomelov <k-udm@ya.ru>
2021-03-09 19:51:36 +0400

    Для подготовки к собеседованию

---
+++ Для подготовки/src/company/chapter1/pack/C1Q16.java
@@ -0,0 +1,16 @@
+package company.chapter1.pack;
+
+/**
+ * Created by Данилов on 19.07.2017.
+ */
+public class C1Q16 {
+    public static void question16() {
+        int a = 0b101;
+        int b = 0xE;
+        double c = 0xE;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ Для подготовки/src/company/chapter2/pack/C2Q02.java
@@ -0,0 +1,22 @@
+package company.chapter2.pack;
+
+/**
+ * Created by Данилов on 19.07.2017.
+ */
+public class C2Q02 {
+    public static void question2() {
+        byte x = 5;
+        byte y = 10;
+
+        int i = x + y;
+
+        long l = x + y;
+
+        double d = x + y;
+
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+
+    }
+}
