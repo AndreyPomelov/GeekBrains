602ffa34858d5f8e0047f176c21635410ac87968
AndreyPomelov <k-udm@ya.ru>
2021-05-09 12:29:28 +0400

    Added display of local files list

--- claudia/client/src/main/java/MainController.java
+++ claudia/client/src/main/java/MainController.java
@@ -1,12 +1,44 @@
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.FileVisitOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ResourceBundle;
 
 // Класс-контроллер основного окна программы
-// (будет отображаться после успешной регистрации или авторизации).
-public class MainController {
+public class MainController implements Initializable {
+
+    public ListView localFilesList;
+    public ListView serverFilesList;
+    private AuthController authController;
+    private final String FILES_DIR_PATH = "client/files/";
 
     public void upload(ActionEvent actionEvent) {
     }
 
     public void download(ActionEvent actionEvent) {
+    }
+
+    public void showFilesLists() {
+        localFilesList.getItems().clear();
+        serverFilesList.getItems().clear();
+        File dir = new File(FILES_DIR_PATH);
+        Platform.runLater(() -> {
+            for (File file : dir.listFiles()) {
+                localFilesList.getItems().add(file.getName());
+            }
+        });
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        authController = AuthController.authController;
+        showFilesLists();
     }
 }
